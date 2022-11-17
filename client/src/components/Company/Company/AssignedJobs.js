b610cd0eaa437b0cacfbdb3b99b25292074a4adc
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-11-16 16:04:28 -0800

    fixed background issue

--- client/src/components/Company/Company/AssignedJobs.js
+++ client/src/components/Company/Company/AssignedJobs.js
@@ -18,7 +18,7 @@ function AssignedJobs({allJobs, deleteJob, technicians, user, sendEmail}) {
   const yourAssignedJobTasks = yourJobTasks.filter(task => task.status !== "Not Assigned")
 
     return (
-      <div className="h-screen bg-blue-400">
+      <div className="h-fit bg-blue-400">
         <h2 className="text-center">Assigned Jobs</h2>
         <div>
           {yourAssignedJobTasks.map((job) => <JobCard key=
